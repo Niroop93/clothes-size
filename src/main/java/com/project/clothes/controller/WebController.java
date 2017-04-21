515a8a69e4cc787d2ee4bad01dea6cab3bfafabe
bmthanki <bhargavthanky@gmail.com>
2017-04-20 20:25:32 -0400

    Changes after class 19/04/2017

--- src/main/java/com/project/clothes/controller/WebController.java
+++ src/main/java/com/project/clothes/controller/WebController.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 import com.project.clothes.model.Brand;
-import com.project.clothes.model.InputData;
+//import com.project.clothes.model.InputData;
 import com.project.clothes.model.Jeans;
 import com.project.clothes.model.OutputData;
 import com.project.clothes.repository.BrandRepository;
@@ -59,64 +59,53 @@ public class WebController {
 		List<Jeans> Jean = (List<Jeans>) jeansRepo.findAll();
 		return Jean;
 	}
-	
+
 	@RequestMapping("/getData")
-	public String getData()
-	{
-		
-		
+	public String getData() {
+
 		return null;
-		
+
 	}
-	
-	/*private void findOneJeans(Long brandId,double hipsMin,double hipsMax,double waistMin,double waistMax) {
-		
-		List<Object[]> results = jeansRepo.findJeans(brandId,hipsMin , hipsMax, waistMin, waistMax);
+
+	private String findOneJeans(Long brandId, double hipsMin, double hipsMax, double waistMin, double waistMax) {
+
+		List<Object[]> results = jeansRepo.findJeans(brandId, hipsMin, hipsMax, waistMin, waistMax);
 		OutputData data = new OutputData();
 		for (Object[] result : results) {
-			data.setBrand((String)result[0]);
-			data.setSize((String)result[1].toString());
+			data.setBrand((String) result[0]);
+			data.setSize((String) result[1].toString());
 		}
 		Gson gson = new Gson();
 		System.out.println(gson.toJson(data));
-		//return gson.toJson(data);
-		 * private String findJeansbyBrand(Long brandId1, Long size, Long brandId2) {
-	}*/
-	
+		return gson.toJson(data);
+	}
+
 	@RequestMapping("/findJeans")
-	public String findJeansSize(@RequestBody String input){
+	public String findJeansSize(@RequestBody String input) {
 		System.out.println("in findJeansbyBrand");
 		System.out.println(input);
+		String results="";
 		HashMap<String, String> holder = new HashMap();
-		if(input != null && !input.isEmpty()){
-	        String[] keyVals = input.split("&");
-	        for(String keyVal:keyVals)
-	        {
-	          String[] parts = keyVal.split("=",2);
-	          holder.put(parts[0],parts[1]);
-	        }
-	        String gender = holder.get("gender");
-	        Long brandId1 = Long.parseLong(holder.get("brand"));
-	        Long size = Long.parseLong(holder.get("size"));
-	        //float hips = Float.valueOf(holder.get("style"));
-	        Long brandId2 = Long.parseLong(holder.get("brand2"));
-		
-				if(brandId1 != null && size != null && brandId2 != null){
-					float waistMin = jeansRepo.findWaistbySize(brandId1, size);
-					float waistMax = waistMin + 0.5f;
-					List<Object[]> results = jeansRepo.findJeansbyBrand(brandId2,waistMin,waistMax);
-					System.out.println(results);
-					if(results != null && !results.isEmpty()){
-						OutputData data = new OutputData();
-						for (Object[] result : results) {
-							data.setBrand((String)result[0]);
-							data.setSize((String)result[1].toString());
-						}
-						Gson gson = new Gson();
-						System.out.println(gson.toJson(data));
-						return gson.toJson(data);
-					}
-				}return null;
-			}return null;
+		if (input != null && !input.isEmpty()) {
+			String[] keyVals = input.split("&");
+			for (String keyVal : keyVals) {
+				String[] parts = keyVal.split("=", 2);
+				holder.put(parts[0], parts[1]);
+			}
+			String gender = holder.get("gender");
+			Long brandId1 = Long.parseLong(holder.get("brand"));
+			Long size = Long.parseLong(holder.get("size"));
+			// float hips = Float.valueOf(holder.get("style"));
+			Long brandId2 = Long.parseLong(holder.get("brand2"));
+
+			if (brandId1 != null && size != null && brandId2 != null) {
+				float waist = jeansRepo.findWaistbySize(brandId1, size);
+				float hips = jeansRepo.findHipbySize(brandId1, size);
+
+				results = this.findOneJeans(brandId2, hips - 0.5f, hips + 0.5f, waist - 0.5f, waist + 0.5f);
+				System.out.println(results);
+			}
 		}
+		return  results;
+	}
 }
